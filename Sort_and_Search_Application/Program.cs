9e0514ee129b06904f5deb0b3f3cdcefccbd99c1
carter stone <stoney300804@gmail.com>
2023-03-17 23:49:11 +0000

    builds 32bit array if integers, sorts in ascending orders, prints the 10th int of each array

--- Sort_and_Search_Application/Program.cs
+++ Sort_and_Search_Application/Program.cs
@@ -6,13 +6,37 @@ namespace ReadTxtFiles
     {
         static void Main(string[] args)
         {
+            //prompt the user to input the text file directory
             Console.WriteLine("Enter the directory containing the text files: ");
             string directoryPath = Console.ReadLine();
 
+            //read each file and create individual arrays
             List<int[]> arrays = new List<int[]>();
             foreach (string filePath in Directory.GetFiles(directoryPath, "*.txt"))
             {
-                string[] lines =  File.ReadAllLines(filePath);
-                int[] array = Array.ConvertAll(lines, int.Parse);
+                string[] lines = File.ReadAllLines(filePath);
+                int[] array = Array.ConvertAll(lines, int.Parse); //convert strings into 32bit integers
                 arrays.Add(array);
             }
+            //sort into ascending order
+            int N = 0;
+            foreach (int[] array in arrays)
+            {
+                for (int i = 0; i < array.Length; i++)
+                {
+                    for (int j = i + 1; j <= array.Length; j++)
+                    {
+                        if (array[i] > array[j])
+                        {
+                            N = array[i];
+                            array[i] = array[j];
+                            array[j] = N;
+                        }
+                    }
+
+                }
+            }
+        }
+    }
+}
+            
