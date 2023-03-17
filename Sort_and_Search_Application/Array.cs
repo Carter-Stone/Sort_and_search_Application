b65a65fd448dffc88a4bf5f6b910b1da8ae8eb5e
stone <stone@LAPTOP-TJHCSMVS>
2023-03-17 15:40:30 +0000

    Add project files.

---
+++ Sort_and_Search_Application/Array.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sort_and_Search_Application
+{
+    class Array
+    {
+        public int[] TrafficData()
+        { 
+            var list = new List<int>();
+            var Road = File.ReadAllLines(Directory + Path);
+        foreach (var line in Road)
+            {
+                list.Add(Convert.ToInt32(line));
+            }
+            return list.ToArray();
+        }
+    }
+}
---
+++ Sort_and_Search_Application/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sort_and_Search_Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
